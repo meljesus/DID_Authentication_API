26a489c81a2dfd02bffe5a4902ba2653a6a663cd
meljesus <73258428+meljesus@users.noreply.github.com>
2025-02-07 14:45:08 +0300

    Backend.js

--- DID/Backend.js
+++ DID/Backend.js
@@ -6,3 +6,10 @@ app.get("/generate-did", (req, res) => {     const wallet =  ethers.Wallet.creat
 const did = `did:ethr:${wallet.address}`;     
 res.json({ did, publicKey: wallet.publicKey, privateKey: wallet.privateKey }); }); 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
+/*
+Yeni bir DID oluşturma
+Kullanıcı için Ethereum tabanlı bir DID üretiriz.
+a.Ethereum için rastgele bir cüzdan oluşturuyoruz.
+b.Cüzdanın adresini DID olarak belirliyoruz.
+*/
